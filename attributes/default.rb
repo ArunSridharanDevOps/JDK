d5268c832914874151eeeb06723664b79bf9810a
ArunSridharanDevOps <arun.sridharan@citi.com>
2016-03-09 18:28:47 -0600

    Update default.rb

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,4 +3,4 @@ default['ihs']['group'] = 'sgsup'
 default['ihs']['root'] = '/apps/ihs-bin'
 default['ihs']['conf_root'] = '/apps/ihs-apps'
 #default['ihs']['sec_root'] = '/opt/ihsSec'
-default[:jdk][:s3_url] = "https://s3-us-west-2.amazonaws.com/ihsbin/agent.installer.linux.gtk.x86_64_1.8.4000.20151125_0201.zip"
+default[:jdk][:s3_url] = "https://s3-us-west-2.amazonaws.com/aruns3/jre-8u73-linux-i586.rpm"
