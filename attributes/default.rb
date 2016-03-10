0aed76d082fc2eea2bb2fcff15d88bd50e4de29e
ArunSridharanDevOps <arun.sridharan@citi.com>
2016-03-09 18:41:01 -0600

    Update default.rb

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,4 +3,5 @@ default['ihs']['group'] = 'sgsup'
 default['ihs']['root'] = '/apps/ihs-bin'
 default['ihs']['conf_root'] = '/apps/ihs-apps'
 #default['ihs']['sec_root'] = '/opt/ihsSec'
+default[:jdk][:rpm_file] = "/apps/jdk.rpm"
 default[:jdk][:s3_url] = "https://s3-us-west-2.amazonaws.com/aruns3/jre-8u73-linux-i586.rpm"
