5c487be0490ddaeb0cc40511e44d55fb5cae72ab
ArunSridharanDevOps <arun.sridharan@citi.com>
2016-03-09 18:01:51 -0600

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -1,4 +1,4 @@
-name             'ihs'
+name             'IHS-CookBook-Citi-rpm-AWS-Discovery'
 maintainer       'YOUR_COMPANY_NAME'
 maintainer_email 'YOUR_EMAIL'
 license          'All rights reserved'
